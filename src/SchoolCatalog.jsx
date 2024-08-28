c7e71a3a7decd53eee2004180494339632b6673b
tiffany <tbcasper16@gmail.com>
2024-08-28 14:14:52 -0500

    first commit and getting started; task one complete and data is now being loaded in from api

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -1,4 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
 export default function SchoolCatalog() {
+  const [courses, updateCourses] = useState([]);
+
+  useEffect(()=> {
+    fetch('/api/courses.json')
+    .then(response => response.json())
+    .then(data => updateCourses(data))
+  }, []);
+  
   return (
     <div className="school-catalog">
       <h1>School Catalog</h1>
@@ -15,36 +25,18 @@ export default function SchoolCatalog() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>PP1000</td>
-            <td>Beginning Procedural Programming</td>
-            <td>2</td>
-            <td>30</td>
-            <td>
+          {courses.map((course, index) => (
+            <tr key={index}>
+              <td>{course.trimester}</td>
+              <td>{course.courseNumber}</td>
+              <td>{course.courseName}</td>
+              <td>{course.semesterCredits}</td>
+              <td>{course.totalClockHours}</td>
+              <td>
               <button>Enroll</button>
             </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>PP1100</td>
-            <td>Basic Procedural Programming</td>
-            <td>4</td>
-            <td>50</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>OS1000</td>
-            <td>Fundamentals of Open Source Operating Systems</td>
-            <td>2.5</td>
-            <td>37.5</td>
-            <td>
-              <button>Enroll</button>
-            </td>
-          </tr>
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className="pagination">
