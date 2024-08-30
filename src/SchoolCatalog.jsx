f118705989e7e54a02d6a5198f5b683d003df1f3
tiffany <tbcasper16@gmail.com>
2024-08-29 19:09:16 -0500

    almost have task 3 done and working, still trying to make the click work properly; pushing work to save progress

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 export default function SchoolCatalog() {
   const [courses, updateCourses] = useState([]);
   const [query, setQuery] = useState("");
+  const [sortCatalog, setSortCatalog] = useState({ key: null, direction: 'ascending'});
 
   useEffect(()=> {
     fetch('/api/courses.json')
@@ -10,10 +11,34 @@ export default function SchoolCatalog() {
     .then(data => updateCourses(data))
   }, []);
 
+  const sortableCourses = React.useMemo(() => {
+    let sortableItems = [...courses];
+    if (sortCatalog.key) {
+      sortableItems.sort((a, b) => {
+        if (a[sortCatalog.key] < b[sortCatalog.key]) {
+          return sortCatalog.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortCatalog.key] > b[sortCatalog.key]) {
+          return sortCatalog.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [courses, sortCatalog]);
+  
   const searchableCourses = courses.filter(course =>
     course.courseNumber.toLowerCase().includes(query.toLowerCase()) ||
     course.courseName.toLowerCase().includes(query.toLowerCase())
   );
+
+  const updateSort = (key) => {
+    let direction = 'ascending';
+    if (sortCatalog.key === key && sortCatalog.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setSortCatalog({ key, direction });
+  };
   
   return (
     <div className="school-catalog">
@@ -22,11 +47,11 @@ export default function SchoolCatalog() {
       <table>
         <thead>
           <tr>
-            <th>Trimester</th>
-            <th>Course Number</th>
-            <th>Courses Name</th>
-            <th>Semester Credits</th>
-            <th>Total Clock Hours</th>
+            <th onClick={() => updateSort('trimester')}>Trimester</th>
+            <th onClick={() => updateSort('courseNumber')}>Course Number</th>
+            <th onClick={() => updateSort('courseName')}>Course Name</th>
+            <th onClick={() => updateSort('semesterCredits')}>Semester Credits</th>
+            <th onClick={() => updateSort('totalClockHours')}>Total Clock Hours</th>
             <th>Enroll</th>
           </tr>
         </thead>
